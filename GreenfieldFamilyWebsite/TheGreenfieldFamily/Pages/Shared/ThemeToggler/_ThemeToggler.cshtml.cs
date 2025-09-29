8d70a90f2613c8d190a509037ae4daedfc39b0b4
Nikolas Greenfield <NikolasGreenfield@gmail.com>
2025-09-29 15:39:10 -0500

    Update Bootstrap and Add Theme Switching
    Updates the bootstrap library to 5.3.8 to allow for simple theme switching.
    Adds a theme switching button to the header, which saves the default theme in local storage across sessions.

---
+++ GreenfieldFamilyWebsite/TheGreenfieldFamily/Pages/Shared/ThemeToggler/_ThemeToggler.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheGreenfieldFamily.Pages.Shared.ThemeToggler
+{
+    public class _ThemeTogglerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
